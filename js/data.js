8b8613b3e73c1e3caff334cb5554cd0fe04b63dd
itsjackie00 <jackiedanso33@gmail.com>
2024-04-03 08:51:29 +0200

    added data array wid id,text and booleando (true/false)

---
+++ js/data.js
@@ -0,0 +1,37 @@
+// Crea un array di dati indicando, ogni todo sarà un oggetto, formato da tre proprietà:
+//id, un numero univoco incrementale
+// text, una stringa che indica il testo del todo
+//done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+
+export const todoList = [
+    {
+        id: 1,
+        text: 'Fare i compiti',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'Fare la spesa',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'Fare il bucato',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'Fare la lavatrice',
+        done: false
+    },
+    {
+        id: 5,
+        text: 'Cucinare la cena',
+        done: false
+    },
+    {
+        id: 6,
+        text: 'Chiamare il dottore',
+        done: false
+    }
+];
