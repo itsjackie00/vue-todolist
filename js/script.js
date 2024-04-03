266bc690e0b6af82242cc700e66a91ad52586d04
itsjackie00 <jackiedanso33@gmail.com>
2024-04-03 09:15:48 +0200

    added toggleDone

--- js/script.js
+++ js/script.js
@@ -3,14 +3,29 @@ import {todoList} from './data.js';
 const {createApp} = Vue;
 
 //crea l'applicazione
-
 createApp({
     data(){
         return{
-            todoList: todoList
+            todoList: todoList,
+            itemText: '',
+            done:'',
         }
     },
     methods: {
+        toggleDone(id){
+            const item = this.todoList.find((el) => {
+                return el.id === id;
+            })
+            console.log(item);
+            if(item){
+                item.done = !item.done;
+            }
+            this.todo[item].done = !this.todo[item].done
+        },
+        
 
+    },
+    mounted(){
+        console.log(this.todoList);
     }
 }).mount('#app')
