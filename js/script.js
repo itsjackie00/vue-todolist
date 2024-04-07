06a2f5860f8587049c311344ad3f4d96824e7795
itsjackie00 <jackiedanso33@gmail.com>
2024-04-07 16:35:27 +0200

    added addTodo function

--- js/script.js
+++ js/script.js
@@ -20,13 +20,29 @@ createApp({
             if (item) {
                 item.done = !item.done;
             }
-            this.todo[item].done = !this.todo[item].done
         },
         removeItem(id) {
             const i = this.todoList.findIndex((el) => el.id === id);
             if (i !== -1) {
                 this.todoList.splice(i, 1);
             }
+        },
+        addTodo (){
+            let nextId = 0;
+            const newTodoList = {
+                id: null,
+                text: this.itemText,
+                done: false
+            }
+            this.todoList.forEach((el) => {
+                if(nextId < el.id){
+                    nextId = el.id;
+                }
+            })
+            newTodoList.id = nextId + 1;
+            this.todoList.push(newTodoList);
+            this.itemText = '';
+            console.log(this.todoList);
         }
 
     },
